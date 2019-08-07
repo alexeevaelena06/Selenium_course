d6e3b90c5f8dec803ab05a5041ec7c468d8e4a5f
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-08-07 22:10:44 +0300

    1) add test_admin_login.py (test to login as admin) 2) add conftest.py, locators.py

---
+++ conftest.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+import threading
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+PROJECT_ROOT = os.path.dirname(__file__)
+DRIVER_DIR = '{}\\chromedriver.exe'.format(str(os.getcwd()))
+DOWNLOADS_DIR = os.path.join(PROJECT_ROOT, 'downloads')
+
+local_data = threading.local()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="available: chrome, remote_chrome"
+    )
+
+
+@pytest.fixture
+def fixture(request):
+    start_browser()
+    request.addfinalizer(local_data.driver.quit)
+    return local_data.driver
+
+
+def start_browser():
+    chrome_options = Options()
+    chrome_options.add_experimental_option('prefs',
+                                           {"download.default_directory": DOWNLOADS_DIR,
+                                            "download.prompt_for_download": False,
+                                            "download.directory_upgrade": True,
+                                            "safebrowsing.enabled": True})
+    driver = webdriver.Chrome(executable_path=DRIVER_DIR, chrome_options=chrome_options)
+    local_data.driver = driver
+    return driver
---
+++ locators.py
@@ -0,0 +1,5 @@
+admin_login = ['http://localhost/litecart/admin/']
+
+admin = {'login_name': 'admin', 'login_password': 'admin'}
+
+
