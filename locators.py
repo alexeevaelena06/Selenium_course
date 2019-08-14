acd7f02d6f58ee34a34d3a821f317fd724a5effa
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-08-14 17:30:55 +0300

    1) add test_all_tabs.py 2) modify locators.py (add locators for test_all_tabs.py)

--- locators.py
+++ locators.py
@@ -2,4 +2,10 @@ admin_login = ['http://localhost/litecart/admin/']
 
 admin = {'login_name': 'admin', 'login_password': 'admin'}
 
+locator_all_tab = ["//*[@id='app-']/a/span[2]"]
+
+locator_sub_tab = ["//ul[@class='docs']//li//span"]
+
+locator_title = [".//*[@id='content']/h1"]
+
 
