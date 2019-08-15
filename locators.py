d6c6535359060ac3a9701260bf2cdc67f09b8853
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-08-15 22:49:12 +0300

    1) add test_check_stickers.py 2) modify locators.py (add locators for test_check_stickers.py)

--- locators.py
+++ locators.py
@@ -1,5 +1,9 @@
 admin_login = ['http://localhost/litecart/admin/']
 
+website_litecart = ['http://localhost/litecart/en/']
+
+locator_litecart = ['div a img[title="My Store"]']
+
 admin = {'login_name': 'admin', 'login_password': 'admin'}
 
 locator_all_tab = ["//*[@id='app-']/a/span[2]"]
@@ -7,5 +11,9 @@ locator_all_tab = ["//*[@id='app-']/a/span[2]"]
 locator_sub_tab = ["//ul[@class='docs']//li//span"]
 
 locator_title = [".//*[@id='content']/h1"]
+
+locator_all_stickers = ["div.sticker"]
+
+locator_all_goods = ["div img.image"]
 
 
