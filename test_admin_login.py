1b207f65313f01620176af0783cb559b3c9875f4
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-08-17 17:51:29 +0300

    1) hw02 - done

---
+++ test_admin_login.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import admin_login, admin
+
+
+def test_admin_login(fixture):
+    driver = fixture
+    driver.get(admin_login[0])
+    driver.find_element_by_name("username").send_keys(admin['login_name'])
+    driver.find_element_by_name("password").send_keys(admin['login_password'])
+    driver.find_element_by_name("login").click()
+    WebDriverWait(driver, 10).until(EC.title_is("My Store"))
