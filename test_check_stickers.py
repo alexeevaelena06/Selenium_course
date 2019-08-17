9266ca37c97a1d488172b8beb1a3e55432619104
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-08-17 17:55:04 +0300

    1) hw04 - done

---
+++ test_check_stickers.py
@@ -0,0 +1,15 @@
+from locators import *
+
+
+def test_check_stickers(fixture):
+    driver = fixture
+    driver.get(website_litecart[0])
+    displayed_stickers = []
+    stickers_list = driver.find_elements_by_css_selector(locator_all_goods[0])
+    for i in range(len(stickers_list)):
+        sticker = driver.find_elements_by_css_selector(locator_all_stickers[0])
+        sticker_name = sticker[i].text
+        assert sticker_name is not None, 'Sticker is absent of image'
+        displayed_stickers.append(sticker_name)
+    print(displayed_stickers)
+    assert len(displayed_stickers) == len(stickers_list)
